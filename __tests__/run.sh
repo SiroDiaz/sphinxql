dcdca58e36217199ed65dd5e0253112df9b96588
Siro Díaz Palazón <sirodiaz93@gmail.com>
2019-06-07 01:45:08 +0200

    Fixed indentation in sphinx.conf

--- __tests__/run.sh
+++ __tests__/run.sh
@@ -14,11 +14,11 @@ case $SEARCH_BUILD in
   MANTICORE2)
     WORK=$HOME/search
     # gcc -shared -o data/test_udf.so ms_test_udf.c
-    $WORK/usr/bin/searchd -c manticore.conf
+    $WORK/usr/bin/searchd -c sphinx.conf
     ;;
   MANTICORE3)
     WORK=$HOME/search
     # gcc -shared -o data/test_udf.so ms_test_udf.c
-    $WORK/usr/bin/searchd -c manticore.conf
+    $WORK/usr/bin/searchd -c sphinx.conf
     ;;
 esac
